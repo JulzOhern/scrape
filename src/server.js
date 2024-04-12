7c72b130cf445addf246d728f175498862c3b6e5
julz <owanojulius742002@gmail.com>
2024-04-12 11:55:14 +0800

    Added manga api

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import { config } from "dotenv";
 import readmRouter from "./routes/manga/readm/index.js";
 import mangafreakRouter from "./routes/manga/mangafreak/index.js";
 import mangamonksRouter from "./routes/manga/mangamonks/index.js";
-import { content } from "./lib/ex.js";
 
 config();
 
@@ -14,7 +13,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (_, res) => res.send(content));
+app.get("/", async (_, res) => res.send(`<h1>Manga scrape</h1>`));
 
 app.use("/readm", readmRouter);
 app.use("/mangafreak", mangafreakRouter);
